8fc50c7a48ed78e6b01b6c8f216830fddaae172f
Tristan Thompson <catsrul10@gmail.com>
2017-01-24 01:39:26 -0800

    Cleaned up

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,7 +16,7 @@ positionDelta speed position time = position + speed * time
 momentum::Double->Double->Double
 momentum speed mass = speed * mass
 
---Is there a better way to do this?
+--True if two bodies exist in the same position. False if not.
 touching::Body->Body->Bool
 touching b1 b2 = (position b1) == (position b2)
 
@@ -24,9 +24,11 @@ touching b1 b2 = (position b1) == (position b2)
 force::Double->Double->Double
 force acceleration mass = mass*acceleration
 
+--Calculate acceleration
 acceleration::Double->Double->Double
 acceleration force mass = force/mass
 
+--Calculate attraction
 attraction::Double->Double->Double->Double->Double
 attraction g mass1 mass2 distance = (g*mass1*mass2) / distance^2
 
@@ -39,21 +41,25 @@ earthPoint position = (StaticPoint (5.972 * 10^24) position)
 -- gravityEffectNormal::Body->Body->Body
 -- gravityEffectNormal = gravityEffect gNormal
 
+--Simulate a body for one tick.
 simulateBody::[Body]->Double -> Body ->Body
 simulateBody _ _ body@(StaticPoint _ _)= body
 simulateBody bodies timeStep body@(Point mass speed accel position) =
-  let attractionForce = foldr (\affectingBody v -> (if affectingBody /= body then (attracts 1 affectingBody body) else 0.0) + v) 0.0 bodies
+  let attractionForce = netAttraction gNormal body bodies --foldr (\affectingBody v -> (if affectingBody /= body then (attracts 1 affectingBody body) else 0.0) + v) 0.0 bodies
       newAcceleration = (acceleration attractionForce mass)
       newSpeed = speedDelta speed newAcceleration timeStep
       newPosition = positionDelta newSpeed position timeStep
   in (Point mass newSpeed newAcceleration newPosition)
 
-attractionForce::Double->Body->[Body] -> Double
-attractionForce g body bodies = foldr (\affectingBody v -> (if affectingBody /= body then (attracts 1 affectingBody body) else 0.0) + v) 0.0 bodies
+--Calculate the sum of the force of the attracting bodies on body
+netAttraction::Double->Body->[Body] -> Double
+netAttraction g body bodies = foldr (\affectingBody v -> (if affectingBody /= body then (attracts g affectingBody body) else 0.0) + v) 0.0 bodies
 
+--Simulate multiple bodies for one tick
 simulateBodies::[Body] -> Double -> [Body]
 simulateBodies bodies timeStep = map (simulateBody bodies timeStep) bodies
 
+--Simulate multiple bodies for a tick size for some amount of ticks. Each array represents the bodies's state at that tick.
 simulate::[Body] -> Double -> Double -> [[Body]]
 simulate _ _ 0 = []
 simulate bodies timeStep iterations = let state = simulateBodies bodies timeStep in state:simulate state timeStep (iterations - 1)
